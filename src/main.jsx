820e53d19cca686477628fbfe08ac98106ea34ef
Naim Jalil <muhammadnaim370@gmail.com>
2025-04-22 01:30:40 +0800

    Enable Analyticss in Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./routes";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
     <SpeedInsights />
   </StrictMode>
 );
