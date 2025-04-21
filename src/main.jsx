b44f0358e3df3cd06ba920a72a85c55cd8f7a716
Naim Jalil <muhammadnaim370@gmail.com>
2025-04-22 01:23:40 +0800

    Enable Speed Insights in Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights />
   </StrictMode>
 );
